0c81372ef846b8100e0c24ebbffc847ca6d7c4ec
Vaibhav Sharma <v.s.10060205@gmail.com>
2025-11-13 20:24:42 +0530

    Todo Made

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { TodoProvider } from './contexts'
+import { TodoForm, TodoItem } from './components/index'
+
+function App() {
+  const [todos, setTodos] = useState([])
+  const addTodo = (todo) => {
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev])  //prev is an array, todo is an object
+  }
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? todo : prevTodo))
+  }
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id != id)) // in deletion from arrays generally prefer filter as it works on true statement
+  }
+
+  const toggleComplete = (id) => {
+    setTodos((prev) => prev.map((todo) => todo.id === id ? { ...todo, completed: !todo.completed } : todo))
+  }
+
+  // function on loading might have some todos stored in local storage. on loading we should be shown those. useEffect will query with local storage, take the todos and update it in todos of useState on loading.
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"))
+    // we have converted this to JSON but ultimately its an array so we checked length.
+    // we are using setTodos only in case todos exist, because if not we don't need to setTodo because by default it is an empty array
+    if (todos && todos.length > 0) {
+      setTodos(todos)
+    }
+  }, [])
+  // we want that if any change happens in todos array, we want to store it in localStorage
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
+
+
+  return (
+    <TodoProvider value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}>
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+            Manage Your Todos
+          </h1>
+          <div className="mb-4">
+            {/*Todo form goes here */}
+            <TodoForm />
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+            {
+              todos.map((todo) => (
+                <div key={todo.id} className="w-full">
+                  <TodoItem todo={todo} />
+                </div>
+              ))
+            }
+          </div>
+        </div>
+      </div>
+    </TodoProvider>
+  )
+}
+
+export default App
---
+++ src/contexts/index.js
@@ -0,0 +1,3 @@
+export { TodoContext, TodoProvider, useTodo } from "./TodoContext"
+
+// as useTodo has the all the components, we can use this in components to filter and use things according out requirement
